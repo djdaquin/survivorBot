aa853f28afcc60d2346a5d82993055c594badef7
David d'Aquin <david@daquin.com>
2016-11-30 18:16:56 -0600

    finished writing tests for contestant model

--- test/contestant-model-spec.js
+++ test/contestant-model-spec.js
@@ -7,7 +7,18 @@ const Contestant = require('../models/contestant.js');
 
 describe('Contestant Model', function () {
   it('should exist', function () {
+    expect(Contestant).to.be.ok;
+  });
+  it('should return something', function () {
     const contestant = new Contestant();
     expect(contestant).to.exist;
+  });
+  it('should return an object', function () {
+    const contestant = new Contestant();
+    expect(contestant).to.be.an('object');
+  });
+  it('should be a \"contestant\"', function () {
+    const contestant = new Contestant();
+    expect(contestant instanceof Contestant).to.be.true;
   });
 });
