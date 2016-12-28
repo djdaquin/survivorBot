6eef4da73d9de6f369ef663a26bc3480b97014a8
David d'Aquin <david@daquin.com>
2016-12-28 17:17:31 -0600

    [minor]changed name

--- models/game.js
+++
@@ -1,7 +0,0 @@
-const Contestant = require('./contestant.js');
-
-const Game = function () {
-}
-
-
-module.exports = Game;
---
+++ models/gamestate.js
@@ -0,0 +1,12 @@
+const Contestant = require('./contestant.js');
+
+// Game should take an array of actions and return a game state.
+// Should it take just actions or actions and characters?
+// Probably game, actions, and characters/contestants
+
+const Game = function () {
+  return {};
+}
+
+
+module.exports = Game;
