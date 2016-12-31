8310d605fe53d4ba7aa47154341e3664a189c6da
David d'Aquin <david@daquin.com>
2016-12-31 16:13:04 -0600

    now handles and passes on game information into gamestate

--- test/game-model-spec.js
+++ test/game-model-spec.js
@@ -9,9 +9,45 @@ describe('GameState Model', function () {
   it('should exist', function () {
     expect(GameState).to.be.ok;
   });
-  const game = GameState();
   it('should return something', function () {
+    const game = GameState();
     expect(game).to.exist;
   });
-  it('should take a ');
+
+  const gameObject = {
+    start: 10,
+    safety: 30,
+    url: 'http://www.reddit.com/r/testing/comments/abc123/survivor',
+    id: 1,
+  };
+  const characters = [
+    {
+      name: 'John Smith',
+      visid: 'johnsm',
+    },
+    {
+      name: 'Spiderman',
+      visid: 'spider',
+    },
+    {
+      name: 'Superman',
+      visid: 'superm',
+    },
+    {
+      name: 'Batman',
+      visid: 'batman',
+    }
+  ];
+  const actions = [];
+
+  it('should have the game information in it', function (){
+    const game = GameState(gameObject, characters, actions);
+    expect(game).to.have.keys('gameHPStart', 'gameHPSafety',
+      'gameURL', 'gameID');
+    expect(game.gameHPStart).to.equal(10);
+    expect(game.gameHPSafety).to.equal(30);
+    expect(game.gameURL).to.equal('http://www.reddit.com/r/testing/comments/abc123/survivor');
+    expect(game.gameID).to.equal(1);
+  });
+
 });
