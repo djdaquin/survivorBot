6822f0015ef69f123b624fcae274b40ab5fb9119
David d'Aquin <david@daquin.com>
2017-01-04 17:03:24 -0600

    created secret keeping file example

---
+++ snoowrap_config/snoowrap-secrets-examples.js
@@ -0,0 +1,10 @@
+// Copy and paste this into a file in this folder called snoowrap-secrets.js.
+// Fill out all the properties
+
+module.exports = {
+  userAgent:
+  clientId:
+  clientSecret:
+  username:
+  password:
+}
