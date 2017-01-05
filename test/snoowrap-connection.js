a19ca02b2479084ac88d8a5e2c35f1d4ee61e461
David d'Aquin <david@daquin.com>
2017-01-05 13:34:26 -0600

    testing for snoowrap

---
+++ test/snoowrap-connection.js
@@ -0,0 +1,18 @@
+/*global describe it */
+
+const chai = require('chai');
+const chaiaspromised = require('chai-as-promised');
+const expect = chai.expect;
+
+chai.use(chaiaspromised);
+
+const r = require('../snoowrap_config/snoowrap.js');
+
+describe('Snoowrap/reddit connection', function () {
+  it('should exist', function () {
+    expect(r).to.be.ok;
+  });
+  it('should return not an error', function () {
+    return expect(r.getInbox()).to.eventually.be.an('array');
+  });
+});
