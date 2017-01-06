35bd1b5579071ce1f7865161b0bad0261b43baba
David d'Aquin <david@daquin.com>
2017-01-06 13:19:41 -0600

    created sample actions

---
+++ test/fixtures/sampleActions.js
@@ -0,0 +1,28 @@
+// These actions go with sampleCharacters
+
+
+const actions = [
+  { healTarget: 'superm', hurtTarget: 'spider', user: '/u/survivorBot'},
+  { healTarget: 'superm', hurtTarget: 'spider', user: '/u/survivorBot'},
+  { healTarget: 'superm', hurtTarget: 'spider', user: '/u/survivorBot'},
+  { healTarget: 'superm', hurtTarget: 'spider', user: '/u/survivorBot'},
+  { healTarget: 'superm', hurtTarget: 'spider', user: '/u/survivorBot'},
+  { healTarget: 'superm', hurtTarget: 'spider', user: '/u/survivorBot'},
+  { healTarget: 'superm', hurtTarget: 'spider', user: '/u/survivorBot'},
+  { healTarget: 'superm', hurtTarget: 'spider', user: '/u/survivorBot'},
+  { healTarget: 'superm', hurtTarget: 'spider', user: '/u/survivorBot'},
+  { healTarget: 'superm', hurtTarget: 'spider', user: '/u/survivorBot'},
+  { healTarget: 'superm', hurtTarget: 'batman', user: '/u/survivorBot'},
+  { healTarget: 'superm', hurtTarget: 'johnsm', user: '/u/survivorBot'},
+  { healTarget: 'superm', hurtTarget: 'batman', user: '/u/survivorBot'},
+  { healTarget: 'superm', hurtTarget: 'johnsm', user: '/u/survivorBot'},
+  { healTarget: 'superm', hurtTarget: 'batman', user: '/u/survivorBot'},
+  { healTarget: 'superm', hurtTarget: 'johnsm', user: '/u/survivorBot'},
+  { healTarget: 'superm', hurtTarget: 'batman', user: '/u/survivorBot'},
+  { healTarget: 'superm', hurtTarget: 'johnsm', user: '/u/survivorBot'},
+  { healTarget: 'superm', hurtTarget: 'batman', user: '/u/survivorBot'},
+  { healTarget: 'superm', hurtTarget: 'johnsm', user: '/u/survivorBot'},
+  { healTarget: 'batman', hurtTarget: 'johnsm', user: '/u/survivorBot'},
+];
+
+module.exports = actions;
