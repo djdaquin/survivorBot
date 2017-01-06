af2bc81f46a9143bdeeff80aa41c9f0c2977116a
David d'Aquin <david@daquin.com>
2017-01-06 12:18:07 -0600

    added test fixtures

---
+++ test/fixtures/sampleCharacters.js
@@ -0,0 +1,20 @@
+const characters = [
+  {
+    name: 'John Smith',
+    visid: 'johnsm',
+  },
+  {
+    name: 'Spiderman',
+    visid: 'spider',
+  },
+  {
+    name: 'Superman',
+    visid: 'superm',
+  },
+  {
+    name: 'Batman',
+    visid: 'batman',
+  }
+];
+
+module.exports = characters;
---
+++ test/fixtures/sampleGameObject.js
@@ -0,0 +1,8 @@
+const gameObject = {
+  start: 10,
+  safety: 30,
+  url: 'http://www.reddit.com/r/testing/comments/abc123/survivor',
+  id: 1,
+};
+
+module.exports = gameObject;
