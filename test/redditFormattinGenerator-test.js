ff7f8d1e9a5a0bacb459a9fe8de1c5a6652d0d21
David d'Aquin <david@daquin.com>
2017-01-10 17:29:48 -0600

    test checks for proper output

--- test/redditFormattinGenerator-test.js
+++ test/redditFormattinGenerator-test.js
@@ -3,12 +3,22 @@
 const chai = require('chai');
 const expect = chai.expect;
 
+const GameState = require('../pipeline/gamestate.js');
 const redditGen = require('../pipeline/redditMDGen.js');
+
+const sampG = require('./fixtures/sampleGameObject.js');
+const sampC = require('./fixtures/sampleCharacters.js');
+const sampA = require('./fixtures/sampleActions.js');
+const gameState = GameState(sampG, sampC, sampA);
 
 describe('The Reddit MarkDown Generator (redditMDGen.js)', function () {
 
   it('should be a thing', function() {
     expect(redditGen).to.be.a('function');
+  });
+
+  it('should return a string', function () {
+    expect(redditGen(gameState)).to.be.a('string');
   });
 
 });
