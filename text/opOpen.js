bd39ce704401d50fe52fdf17642e9847c8596036
David d'Aquin <david@daquin.com>
2017-01-10 17:18:44 -0600

    converted opening of op md to javascript string

---
+++ text/opOpen.js
@@ -0,0 +1,33 @@
+var text = '';
+
+text = '# Welcome to the Hurt and Heal Games \
+\n\n  \
+This game is about finding the ultimate survivor from a list of...well however \
+many! Every turn you will be able to **HURT** one character and **HEAL** another, \
+appropriately lowering their hp by one or raising it by one. Whenever a character\'s \
+hp hits 0, they are removed from the list. Whenever a character\'s hp hits a \
+safety mark of 30, they are moved to a "haven" that prevents them from being \
+affected until there is only one person left in the main area...in which case all characters \
+in the haven are moved back into the fray back at the base hp. This continues \
+until there is only one character left, with that character being named \
+the winner! \
+\n\n\  \
+In order to participate, all you have to do is reply to this op in the following \
+manor: \
+ \
+``` \
+++{character you want to heal\'s id} \
+--{character you want to hurt\'s id} \
+``` \
+\n\n \
+You can comment once every 4 hours! Use it wisely! You can check if you are on \
+cooldown below. \
+\n\n \
+Without further to do, here are your contestants! \
+';
+
+
+
+console.log(text);
+
+module.exports = text;
