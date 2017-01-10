2e2b2f166c3ff4874704a0af074dbd696ba2d894
David d'Aquin <david@daquin.com>
2017-01-10 17:28:40 -0600

    generator now grabs opening text

--- pipeline/redditMDGen.js
+++ pipeline/redditMDGen.js
@@ -1,7 +1,14 @@
 
 
-const redditFormattingGenerator = function () {
+const redditFormattingGenerator = function (gameState) {
+  let markdown = ''
 
-}
+  markdown = markdown + require('../text/opOpen.js');
+
+
+  return markdown;
+};
+
+console.log(redditFormattingGenerator());
 
 module.exports = redditFormattingGenerator;
