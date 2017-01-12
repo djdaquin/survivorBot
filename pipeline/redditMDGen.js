7f72632689fa1765667594892e07dd4d4b3d5855
David d'Aquin <david@daquin.com>
2017-01-12 15:29:32 -0600

    properly prints out contestants that are alive

--- pipeline/redditMDGen.js
+++ pipeline/redditMDGen.js
@@ -1,14 +1,22 @@
 
-
 const redditFormattingGenerator = function (gameState) {
-  let markdown = ''
+  let markdown = '';
 
+  // Check if there is a winner or not.
+    // If there is, display a message with a game summary.
+    // If not, make the MD to show the gamestate.
+
+  // Show the open to a gamestate text.
   markdown = markdown + require('../text/opOpen.js');
+  // Create the alive player pool.
+  markdown = markdown + '\n\n|ID|Name|HP|\n|--|----|--|';
+  gameState.characters.alive.forEach(c => {
+    markdown = markdown + '\n|' + c.visid + '|' + c.name + '|' + c.hp;
+  });
 
-
+  console.log(markdown);
   return markdown;
 };
 
-console.log(redditFormattingGenerator());
 
 module.exports = redditFormattingGenerator;
