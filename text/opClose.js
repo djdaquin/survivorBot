6d044a8bc89b5749e12745bd9002eaf28275d138
David d'Aquin <david@daquin.com>
2017-01-12 19:14:07 -0600

    closing text created

---
+++ text/opClose.js
@@ -0,0 +1,7 @@
+var text = '';
+
+text = text + '---\
+\n\nThis is a bot. If you have any questions or I begin acting erratically, please\
+ talk to my creator, /u/mrcelophane.';
+
+module.exports = text;
