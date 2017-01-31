b939f47baf876b4ffa97c82be37192e32b895315
David d'Aquin <david@daquin.com>
2017-01-31 16:16:16 -0600

    added opening text to winner message

---
+++ text/opOpenWinner.js
@@ -0,0 +1,19 @@
+var text = '';
+
+text = '# Welcome to the Hurt and Heal Games \
+\n\n\
+This game is about finding the ultimate survivor from a list of...well however \
+many! Every turn you will be able to **HURT** one character and **HEAL** another, \
+appropriately lowering their hp by one or raising it by one. Whenever a character\'s \
+hp hits 0, they are removed from the list. Whenever a character\'s hp hits a \
+safety mark of 30, they are moved to a "haven" that prevents them from being \
+affected until there is only one person left in the main area...in which case all characters \
+in the haven are moved back into the fray back at the base hp. This continues \
+until there is only one character left, with that character being named \
+the winner! \
+\n\n\
+A winner has already been named in this game! And the winner is:\
+\n\n\
+##'
+
+module.exports = text;
